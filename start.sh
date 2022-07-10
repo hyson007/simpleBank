6dee0f00640dcfa3d1e040b122343aae11c9907f
Jack Yao <hyson007@gmail.com>
2022-07-10 09:19:20 +0800

    load environment test

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ set -e
 # to let script exit immediately if command return non-zero
 
 echo "run db migration"
+ls -al /app/app.env
+cat /app/app.env
 source /app/app.env
 env
 /app/migrate -path /app/migration -database "$DBSOURCE" -verbose up
